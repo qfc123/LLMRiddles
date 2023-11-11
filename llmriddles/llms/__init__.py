51c344c7e3a0e31d6f9e29accc67f328ecbf3dd3
qfc123 <565501125@qq.com>
2023-11-11 17:19:16 +0800

    Update __init__.py

--- llmriddles/llms/__init__.py
+++ llmriddles/llms/__init__.py
@@ -1,4 +1,5 @@
 from .base import register_llm, get_llm_fn
 from .chatgpt import ask_chatgpt
+from .baidu import ask_baidu
 from .chatglm import ask_chatglm
 from .mistral import ask_mistral_7b_instruct
